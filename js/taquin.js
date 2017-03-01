57b4123d52ae4f336c5f5d8fdbf9c78a3a6f9da3
arnaudgregoire <arnaud.gregoire@ensg.eu>
2017-03-01 22:56:08 +0100

    ajout mode géographique + drawImage

--- js/taquin.js
+++ js/taquin.js
@@ -4,7 +4,7 @@ var img         = new Image();
 var gris        = new Image();
 var canvas      = document.getElementById('canvas');
 var context     = canvas.getContext('2d');
-img.src         = "img/pissaro.jpg";
+img.src         = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center=37.530101,38.600062&zoom=14&size=600x600&key=" + api_key;
 gris.src        = "img/gris.jpg";
 L_img           = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 var img_ready   = false;
@@ -15,51 +15,76 @@ var step        = 0;
 
 img.addEventListener('load', function() {
   // on attend que notre image principale ait été chargé et on la dessine dans notre canvas
-        context.drawImage(img,0,0,600,600);
+        context.drawImage(img,0,0,600,600,0,0,600,600);
         img_ready = true;
         init();
     });
 
 gris.addEventListener('load',function(){
   // on attend que le petit carré gris mobile ait été chargé et on le dessine dans notre canvas
-        context.drawImage(gris,0,0);
+        context.drawImage(gris,0,0,100,100,0,0,100,100);
         gris_ready = true;
         init();
 })
+function create_url(lat,long) {
+      var url = "https://maps.googleapis.com/maps/api/staticmap?maptype=satellite&center="
+      url    +=  lat  + ","
+      url    +=  long
+      url    +=  "&zoom=14&size=600x600&key="
+      url    +=  api_key
+      console.log(url);
+      return url
+}
 
-
+function create_img() {
+      img.src = create_url(47.613690, 1.368898)
+}
 
 function init() {
   // on initalise toutes nos fonctions une fois que les images sont chargés
   if(img_ready && gris_ready){
+        L_img     = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+        L_history = [];
         create_objs();
         do_bordel();
-        draw_image();
+        draw();
         step = L_history.length-1;
-        console.log(L_img);
+        console.log(gris_x,gris_y);
   }
 }
 
 function to_right(){
   //pour faire aller le carré gris à droite
     if(gris_x<3){
-        temp                           = L_img[gris_x + 1][gris_y].data
-        L_img[gris_x + 1][gris_y].data = L_img[gris_x][gris_y].data
-        L_img[gris_x][gris_y].data     = temp
-        gris_x                        += 1
+        var temp_x                    = L_img[gris_x][gris_y].x
+        var temp_y                    = L_img[gris_x][gris_y].y
+
+        L_img[gris_x][gris_y].x       = L_img[gris_x+1][gris_y].x
+        L_img[gris_x][gris_y].y       = L_img[gris_x+1][gris_y].y
+
+        L_img[gris_x+1][gris_y].x     = temp_x
+        L_img[gris_x+1][gris_y].y     = temp_y
+
+        gris_x                       += 1
         L_history.push(to_left)
-        draw_image()
+        draw()
       }
 }
 
 function to_left(){
   //pour faire aller le caré gris à gauche
     if(gris_x>0){
-        temp                           = L_img[gris_x - 1][gris_y].data
-        L_img[gris_x - 1][gris_y].data = L_img[gris_x][gris_y].data
-        L_img[gris_x][gris_y].data     = temp
-        gris_x                        -= 1
-        draw_image()
+        var temp_x                    = L_img[gris_x][gris_y].x
+        var temp_y                    = L_img[gris_x][gris_y].y
+
+        L_img[gris_x][gris_y].x       = L_img[gris_x-1][gris_y].x
+        L_img[gris_x][gris_y].y       = L_img[gris_x-1][gris_y].y
+
+        L_img[gris_x-1][gris_y].x     = temp_x
+        L_img[gris_x-1][gris_y].y     = temp_y
+
+        gris_x                     -= 1
+        draw()
         L_history.push(to_right)
       }
 }
@@ -67,11 +92,17 @@ function to_left(){
 function to_up(){
   // pour faire monter le carré gris en haut
   if(gris_y>0){
-        temp                           = L_img[gris_x][gris_y - 1].data
-        L_img[gris_x][gris_y - 1].data = L_img[gris_x][gris_y].data
-        L_img[gris_x][gris_y].data     = temp
-        gris_y                        -= 1
-        draw_image()
+        var temp_x                      = L_img[gris_x][gris_y].x
+        var temp_y                      = L_img[gris_x][gris_y].y
+
+        L_img[gris_x][gris_y].y       = L_img[gris_x][gris_y-1].y
+        L_img[gris_x][gris_y].x       = L_img[gris_x][gris_y-1].x
+
+        L_img[gris_x][gris_y-1].x     = temp_x
+        L_img[gris_x][gris_y-1].y     = temp_y
+
+        gris_y                       -= 1
+        draw()
         L_history.push(to_down)
       }
 }
@@ -79,23 +110,60 @@ function to_up(){
 function to_down(){
   // pour faire descendre le carré gris en bas
   if(gris_y<3){
-        temp                           = L_img[gris_x][gris_y + 1].data
-        L_img[gris_x][gris_y + 1].data = L_img[gris_x][gris_y].data
-        L_img[gris_x][gris_y].data     = temp
-        gris_y                        += 1
-        draw_image()
+      var temp_x                      = L_img[gris_x][gris_y].x
+      var temp_y                      = L_img[gris_x][gris_y].y
+
+      L_img[gris_x][gris_y].y       = L_img[gris_x][gris_y+1].y
+      L_img[gris_x][gris_y].x       = L_img[gris_x][gris_y+1].x
+
+      L_img[gris_x][gris_y+1].x     = temp_x
+      L_img[gris_x][gris_y+1].y     = temp_y
+
+      gris_y                       += 1
+        draw()
         L_history.push(to_up)
       }
 }
+
+function draw() {
+  console.log(gris_x,gris_y);
+  draw_image()
+  draw_gris()
+}
+
 
 function draw_image(){
   // on dessine l'image par l'intermédiaire de nos objets "petites images"
   context.clearRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < 4; i++) {
       for (var j = 0; j < 4; j++) {
-          context.putImageData(L_img[i][j].data,L_img[i][j].x,L_img[i][j].y)
+          context.drawImage(img,
+            L_img[i][j].x,
+            L_img[i][j].y,
+            150,
+            150,
+            L_img[i][j].x_draw,
+            L_img[i][j].y_draw,
+            150,
+            150);
       }
   }
+}
+function draw_gris(){
+  context.clearRect(gris_x*150,
+                    gris_y*150,
+                    150,
+                    150)
+
+  context.drawImage(gris,
+                    0,
+                    0,
+                    150,
+                    150,
+                    gris_x*150,
+                    gris_y*150,
+                    150,
+                    150)
 }
 
 function create_objs(){
@@ -103,9 +171,12 @@ function create_objs(){
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             L_img[i][j] = {
-                  x    : i * canvas.width  / 4, // la position où peindre l'image
-                  y    : j * canvas.height / 4,
-                  data : context.getImageData(i*canvas.width/4 , j*canvas.height/4 ,150,150)}; // la matrice de pixel à peindre
+                  x       : i * canvas.width  / 4, // la position où peindre l'image
+                  y       : j * canvas.height / 4,
+                  x_draw  : i * canvas.width  / 4,
+                  y_draw  : j * canvas.height / 4
+                  //data : context.getImageData(i*canvas.width/4 , j*canvas.height/4 ,150,150)}; // la matrice de pixel à peindre
+                 }
         }
     }
 }
@@ -115,7 +186,6 @@ function do_bordel() {
   // un ensemble de mouvements aléatoires compris entre 20 et 40
     var random_1 = Math.floor((Math.random() * 20) + 20);
     var random_2 = Math.floor((Math.random() * 5));
-    console.log(random_1,random_2);
     for (var i = 0; i < random_1; i++) {
       random_2 = Math.floor((Math.random() * 4));
       switch (random_2) {
